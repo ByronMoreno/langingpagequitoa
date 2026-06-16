878dcf2446ae831712ccb6f91cf5ec1c5a776938
ByronMoreno <Byron.moreno.dlu@hotmail.com>
2026-06-16 11:16:51 -0500

    Probando servidor ssh puerto 1985

--- app.py
+++ app.py
@@ -183,6 +183,6 @@ setInterval(actualizarReloj,1000);
 @app.route("/")
 def home():
     return render_template_string(HTML)
-
+#Pruebas
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
